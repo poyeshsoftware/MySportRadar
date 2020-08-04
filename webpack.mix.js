7700c2f37cd73485c4fe8ca96ee67b5936b79824
Barzan <payamprivate2@gmail.com>
2020-08-04 20:30:02 +0430

    Adding Tailwind , and vue components that wee need to project, and configure webpack.mix.js

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,5 @@
 const mix = require('laravel-mix');
+const tailwindcss = require('tailwindcss');
 
 /*
  |--------------------------------------------------------------------------
@@ -11,5 +12,12 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css');
+mix.js('resources/js/main/main.js', 'public/js/main');
+
+mix.js('resources/js/dashboard/main.js', 'public/js/dashboard');
+
+mix.sass('resources/sass/app.scss', 'public/sass')
+    .options({
+        processCssUrls: false,
+        postCss: [tailwindcss('./tailwind.config.js')],
+    });
